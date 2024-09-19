ffd7ee3d5b5fcaeb9e9717bae1dd0aa835c43186
Arunanshu Biswas <mydellpc07@gmail.com>
2024-09-19 20:52:44 +0530

    docs(cli/signal): debug log signal event info and rename enum members

--- crates/cli/src/signals.rs
+++ crates/cli/src/signals.rs
@@ -1,19 +1,21 @@
 use crate::error::Error;
 use flume::Sender;
 use tokio::signal::unix::{signal, SignalKind};
+use tracing::debug;
 
 /// Indefinitely listens to signals and sends signal events to the provided channel.
-pub async fn wait_for_signal(signal_event: &Sender<SignalEvent>) -> Result<(), Error> {
+pub async fn wait_for_signal(signal_event: Sender<SignalEvent>) -> Result<(), Error> {
     let mut sigusr1 = signal(SignalKind::user_defined1()).map_err(Error::SignalHandler)?;
     let mut sigusr2 = signal(SignalKind::user_defined2()).map_err(Error::SignalHandler)?;
+    debug!(?sigusr1, ?sigusr2, "Signal handlers registered");
 
     loop {
         tokio::select! {
             _ = sigusr1.recv() => {
-                signal_event.send_async(SignalEvent::SigUSR1).await?;
+                signal_event.send_async(SignalEvent::DumpStateInfo).await?;
             }
             _ = sigusr2.recv() => {
-                signal_event.send_async(SignalEvent::SigUSR2).await?;
+                signal_event.send_async(SignalEvent::ManualSaveState).await?;
             }
         }
     }
@@ -21,6 +23,8 @@ pub async fn wait_for_signal(signal_event: &Sender<SignalEvent>) -> Result<(), E
 
 #[derive(Debug, Clone, Copy)]
 pub enum SignalEvent {
-    SigUSR1,
-    SigUSR2,
+    /// Dump the current state and related statistics.
+    DumpStateInfo,
+    /// Manually save the current state.
+    ManualSaveState,
 }
